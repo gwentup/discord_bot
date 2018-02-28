51d4c29c986f0ae656be79db0272f8c061689e93
lrossy <lrossy@gmail.com>
2018-02-27 19:59:35 -0500

    Reformat the profiel command Support for PRO

--- core/bot.js
+++ core/bot.js
@@ -18,20 +18,18 @@ client.on('ready', function (evt) {
 });
 
 client.on('message', msg => {
-  let message = msg.content;
+  let message = msg.content.toLowerCase();
   if (message.substring(0, 1) == '!') {
     var args = message.substring(1).split(' ');
     var cmd = args[0];
     args = args.splice(1);
+    let profile_name = args[0] ? args[0]: false;
 
     switch (cmd) {
       case 'ping':
         msg.channel.send(`${msg.member} Pong.`);
         break;
-
       case 'profile':
-        let profile_name = args[0] ? args[0]: false;
-
         if(profile_name){
             var options = {
               uri: 'https://gwentup.com/api/bot/player/find',
@@ -48,17 +46,24 @@ client.on('message', msg => {
               .then((result) => {
 
                 let resp = result[0];
-                console.log('res', resp.type)
                 //could not find, or some other issue
                 if (resp.type === 'BOT_FIND_PLAYER_ERROR') {
                   msg.channel
                     .send(`There was a problem finding your profile. Please try again later`)
                 }
                 else if(resp.type === 'BOT_FIND_PLAYER_SUCCESS'){
-                  msg.channel.send(`${msg.member}`);
+                  // msg.channel.send(`${msg.member}`);
                   let d = resp.data;
                   let res = d.results[0];
-                  msg.channel.send({embed: {
+                  let fields = d.results.map( ele => {
+                    return {
+                      name: ele.OnlineMode === 1 ? 'Ranked': 'Pro',
+                      value: `MMR: ${ele.Mmr} #${ele.Position}\nW/L/D: ${ele.Win}/${ele.Lose}/${ele.Draw} (${ele.WinRate}%)`
+                    };
+                  });
+
+                  msg.channel.send(msg.member, {
+                    embed: {
                     color: 0xdfda13,
                     author: {
                       name: client.user.username,
@@ -68,14 +73,7 @@ client.on('message', msg => {
 
                     url: resp.data.url,
                     // description: "The following commands are available",
-                    fields: [
-                      {name: `Position`, value: res.Position},
-                      {name: `MMR`, value: res.Mmr},
-                      {name: `WINS`, value: res.Win},
-                      {name: `LOSES`, value: res.Lose},
-                      {name: `DRAWS`, value: res.Draw},
-                      {name: `Win Rate`, value: res.WinRate}
-                    ],
+                    fields,
                     timestamp: new Date(),
                     footer: {
                       icon_url: icon,
@@ -84,14 +82,23 @@ client.on('message', msg => {
                   }
                   });
                 }
-              });
+              })
+              .catch( err  => {
+                console.log('err', err.error);
+                if(err.error && err.error[0] && err.error[0].type === 'BOT_FIND_PLAYER_ERROR'){
+                  msg.channel.send(`${msg.member} ${profile_name} was not found`);
+                }
+                else{
+                  msg.channel.send(`${msg.member} There was a problem, please try again later`);
+                }
+              })
 
           break;
         }
 
       default :
-        msg.channel.send({embed: {
-          color: 3447003,
+        msg.channel.send(msg.member, {embed: {
+          color: 0xdfda13,
           author: {
             name: client.user.username,
             icon_url: icon
@@ -112,82 +119,6 @@ client.on('message', msg => {
         }
         });
         break;
-      // case 'leaders':
-      //   let ss_index = args[0] ? args[0]: '6';
-      //   let ss_year = args[1] ? args[1]: '18';
-      //   var options = {
-      //     uri: 'https://gwentup.com/api/report/report',
-      //     qs: {
-      //       year: ss_year,
-      //       index: ss_index
-      //     },
-      //     headers: {
-      //       'User-Agent': 'discord-bot'
-      //     },
-      //     json: true
-      //   };
-      //   var data = [];
-      //   rp(options)
-      //     .then(function (result) {
-      //       let rep = result[1];
-      //       if(rep.type === 'REPORT_SUCCESS'){
-      //         data = rep.report.Data.Leaders[3];
-      //         let leaderIds = rep.report.Data.Leaders[3].map( (e,idx) => {
-      //           return e._id;
-      //         });
-      //
-      //         logger.info(`Fetching from: https://gwentup.com/api/cards/${leaderIds.join(',')}`)
-      //         let opts2 = {
-      //           uri: `https://gwentup.com/api/cards/${leaderIds.join(',')}`,
-      //           headers: {
-      //             'User-Agent': 'discord-bot'
-      //           },
-      //           json: true // Automatically parses the JSON string in the response
-      //         };
-      //         return rp(opts2)
-      //       }
-      //       else return Promise.reject('There was a problem connecting to the API, try again in a few minutes')
-      //     })
-      //     .then( cardResponse => {
-      //       let cardsData = cardResponse[0];
-      //       if(cardsData.type === 'CARDS_SUCCESS'){
-      //         let output = data.map( (ele, idx) => {
-      //           return {
-      //             leaderId: ele._id,
-      //             leaderName: cardsData.cards[ele._id].Name,
-      //             faction: cardsData.cards[ele._id].FactionId,
-      //             winrate: ele.WinRate
-      //           }
-      //         });
-      //         output.sort(function(a,b) {return (a.winrate < b.winrate) ? 1 : ((b.winrate < a.winrate) ? -1 : 0);} );
-      //
-      //         msg.channel.send({embed: {
-      //           color: 3447003,
-      //           author: {
-      //             name: client.user.username,
-      //             icon_url: client.user.avatarURL
-      //           },
-      //           title: `Leaders from Meta Snapshot ${ss_index}`,
-      //           url: `https://gwentup.com/report/18/${ss_index}`,
-      //           description: "Leaders Meta report for year ${} and ",
-      //           fields: output.map( e => { return {name: e.leaderName, value: `Winrate: ${e.winrate} %`}}),
-      //           timestamp: new Date(),
-      //           footer: {
-      //             icon_url: client.user.avatarURL,
-      //             text: "© Gwentup.com"
-      //           }
-      //         }
-      //         });
-      //
-      //       }
-      //       else return Promise.reject('There was a problem connecting to the API, try again in a few minutes')
-      //     })
-      //     .catch(function (err) {
-      //       // API call failed...
-      //       logger.error('Problem!', err.error[0]);
-      //       msg.channel.send('Something went wrong... please try again in a few minutes. ');
-      //
-      //     });
     }
   }
 
